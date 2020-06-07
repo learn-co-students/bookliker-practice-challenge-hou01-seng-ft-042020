44c30a6f67ca98729c910faf3672d7a82256ad68
estherkimyunjung <estherkimyunjung@gmail.com>
2020-06-07 10:18:52 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ fetch(url)
 .then(res => res.json())
 .then(books => {
   for(const book of books){
-    getBooks(book)
+    createBook(book)
   }
 })
 
-function getBooks(book){
+function createBook(book){
   const liBook = document.createElement('li')
   const h3Title = document.createElement('h3')
   h3Title.innerText = book.title
@@ -37,11 +37,29 @@ function getBooks(book){
 
     btnFovor.addEventListener('click',() => {
       user = {"id":1, "username":"pouros"}
-
       book.users.push(user)
+
       let userBook = document.createElement('li')
       userBook.innerText = `${user["username"]}`      
       usersBook.append(userBook)
+
+      const options = {
+        method: "PATCH",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          users: book.users
+        })
+      }
+
+      fetch(url, options)
+      .then(res => res.json())
+      .then(json => {
+
+  
+      })
 
     })
 
@@ -55,6 +73,26 @@ function getBooks(book){
     divShow.append(h2Title, pDesc, imgBook, usersBook, btnFovor)
 
   })
+
+  // const options = {
+  //   method: "POST",
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Accept': 'application/json'
+  //   },
+  //   body: JSON.stringify({
+  //     title: book.title,
+  //     description: book.description,
+  //     img_url: book.img_url,
+  //     users: book.users
+  //   })
+  // }
+
+  // fetch(url, options)
+  // .then(res => res.json())
+  // .then(updatedBook =>{
+  //   createBook(book)
+  // })
 
 }
   
